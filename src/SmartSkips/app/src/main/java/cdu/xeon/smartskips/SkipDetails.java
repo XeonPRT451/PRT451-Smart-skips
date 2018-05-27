607a5cb0c95a9719d65c11eb0293e24bfb577135
JerryKang666 <bashen6666@gmail.com>
2018-05-27 21:40:49 +0930

    17/5/2018  Jerry

--- src/SmartSkips/app/src/main/java/cdu/xeon/smartskips/SkipDetails.java
+++ src/SmartSkips/app/src/main/java/cdu/xeon/smartskips/SkipDetails.java
@@ -3,24 +3,29 @@ package cdu.xeon.smartskips;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 
-public class SkipDetails extends AppCompatActivity {
+import java.util.ArrayList;
 
+import cdu.xeon.data.bean.Operator;
+import cdu.xeon.data.repository.Repository;
+
+public class SkipDetails extends AppCompatActivity {
+    SmsManager smsManager = SmsManager.getDefault();
+    String content = "Hello World!";
+    ArrayList<String> list = smsManager.divideMessage(content);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skip_details);
-
-        findViewById(R.id.userProfileButton).setOnClickListener(new View.OnClickListener() {
+        final Operator operator=Repository.getOperator(this,1).get(0);
+        findViewById(R.id.SMSButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setClass(SkipDetails.this,UserProfile.class );
-                startActivity(intent);
-                int version = Integer.valueOf(android.os.Build.VERSION.SDK);
-                if(version >= 5) {
-                    overridePendingTransition(R.anim.zoomin, R.anim.zoomout);
+
+                for (int i = 0; i < list.size(); i++) {
+                    smsManager.sendTextMessage(operator.getPhone(), null, list.get(i), null, null);
                 }
             }
         });
